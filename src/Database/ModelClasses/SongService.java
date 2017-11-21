1647043c63eb2fcce027cb7097c0e67486511d35
TomDreys <tdreys.61735@farnborough.ac.uk>
2017-11-21 14:19:49 +0000

    packages

--- src/Database/ModelClasses/SongService.java
+++ src/Database/ModelClasses/SongService.java
@@ -11,7 +11,7 @@ public class SongService {
 
     public static void selectAll(List<Song> targetList, DatabaseConnection database) {
 
-        PreparedStatement statement = database.newStatement("SELECT songID, FileURL, SongTitle, SongAlbum, ReleaseYear, TrackNumber FROM Table ORDER BY x");
+        PreparedStatement statement = database.newStatement("SELECT songID, FileURL, SongTitle, SongAlbum, ReleaseYear, TrackNumber FROM Songs ORDER BY x");
 
         try {
             if (statement != null) {
@@ -34,7 +34,7 @@ public class SongService {
 
         Song result = null;
 
-        PreparedStatement statement = database.newStatement("SELECT x, y, z FROM Table WHERE id = ?");
+        PreparedStatement statement = database.newStatement("SELECT SongID, FileURL, SongTitle, SongAlbum, ReleaseYear, TrackNumber FROM Songs WHERE id = ?");
 
         try {
             if (statement != null) {
@@ -56,7 +56,7 @@ public class SongService {
 
     public static void deleteById(int id, DatabaseConnection database) {
 
-        PreparedStatement statement = database.newStatement("DELETE FROM Table WHERE id = ?");
+        PreparedStatement statement = database.newStatement("DELETE FROM Songs WHERE id = ?");
 
         try {
             if (statement != null) {
@@ -75,7 +75,7 @@ public class SongService {
 
         try {
             if (existingItem == null) {
-                PreparedStatement statement = database.newStatement("INSERT INTO Table (songID, FileURL, SongTitle, SongAlbum, ReleaseYear, TrackNumber) VALUES (?, ?, ?, ?, ?, ?))");
+                PreparedStatement statement = database.newStatement("INSERT INTO Songs (songID, FileURL, SongTitle, SongAlbum, ReleaseYear, TrackNumber) VALUES (?, ?, ?, ?, ?, ?))");
                 statement.setInt(1, itemToSave.getSongID());
                 statement.setString(2, itemToSave.getFileURL());
                 statement.setString(3, itemToSave.getSongTitle());
@@ -85,7 +85,7 @@ public class SongService {
                 database.executeUpdate(statement);
             }
             else {
-                PreparedStatement statement = database.newStatement("UPDATE Table SET FileURL = ?, SongTitle = ?, SongAlbum = ?, ReleaseYear = ?, TrackNumber = ? WHERE songID = ?");
+                PreparedStatement statement = database.newStatement("UPDATE Songs SET FileURL = ?, SongTitle = ?, SongAlbum = ?, ReleaseYear = ?, TrackNumber = ? WHERE songID = ?");
                 statement.setString(1, itemToSave.getFileURL());
                 statement.setString(2, itemToSave.getSongTitle());
                 statement.setInt(3, itemToSave.getSongAlbum());
