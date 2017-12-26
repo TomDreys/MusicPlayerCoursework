1f5b61698202d11e56b8e87afb2910f8302d3d39
TomDreys <tdreys.61735@farnborough.ac.uk>
2017-12-26 18:29:38 +0000

    added button labels and table columns

--- src/GUI/GUIBuilder.java
+++ src/GUI/GUIBuilder.java
@@ -64,8 +64,18 @@ public class GUIBuilder {
     public static VBox createLeftVBox(GUI gui)
     {
         VBox leftVbox = new VBox();
+
         HBox leftOptionBar = createLeftOptionBar(gui);
+
         TableView songs = new TableView();
+        TableColumn trackNumber = new TableColumn("#");
+        TableColumn song = new TableColumn("Song");
+        TableColumn artist = new TableColumn("Artist");
+        TableColumn album = new TableColumn("Album");
+        TableColumn length = new TableColumn("Length");
+        TableColumn addDate = new TableColumn("Add Date");
+        songs.getColumns().addAll(trackNumber,song,artist,album,length,addDate);
+
         HBox visualisationTab = createVisualiationTab(gui);
 
         leftVbox.getChildren().add(leftOptionBar);
@@ -77,10 +87,16 @@ public class GUIBuilder {
     public static HBox createLeftOptionBar(GUI gui)
     {
         HBox LeftOptionBar = new HBox();
+
         TextField searchInputField = new TextField();
+
         ChoiceBox searchTypeChoice = new ChoiceBox();
+
         Button searchEnterButton = new Button();
+        searchEnterButton.setText("Search");
+
         Button addNewSongButton = new Button();
+        addNewSongButton.setText("Add Song");
 
         LeftOptionBar.getChildren().add(searchInputField);
         LeftOptionBar.getChildren().add(searchTypeChoice);
@@ -96,21 +112,30 @@ public class GUIBuilder {
         Label albumArtLabel = new Label();
         Slider slider1 = new Slider();
         slider1.setOrientation(Orientation.VERTICAL);
+
         Slider slider2 = new Slider();
         slider2.setOrientation(Orientation.VERTICAL);
+
         Slider slider3 = new Slider();
         slider3.setOrientation(Orientation.VERTICAL);
+
         Slider slider4 = new Slider();
         slider4.setOrientation(Orientation.VERTICAL);
+
         Slider slider5 = new Slider();
         slider5.setOrientation(Orientation.VERTICAL);
+
         Slider slider6 = new Slider();
         slider6.setOrientation(Orientation.VERTICAL);
+
         Slider slider7 = new Slider();
         slider7.setOrientation(Orientation.VERTICAL);
+
         Slider slider8 = new Slider();
         slider8.setOrientation(Orientation.VERTICAL);
+
         Button channelButton = new Button();
+        channelButton.setText("Stereo");
 
         visualiationTab.getChildren().add(albumArtLabel);
         visualiationTab.getChildren().addAll(slider1,slider2,slider3,slider4,slider5,slider6,slider7,slider8);
@@ -122,8 +147,16 @@ public class GUIBuilder {
     public static VBox createRightVBox(GUI gui)
     {
         VBox rightVbox = new VBox();
+
         Button addPlaylistButton = new Button();
+        addPlaylistButton.setText("New Playlist");
+
         TableView playlists = new TableView();
+        TableColumn playlist = new TableColumn("Playlist");
+        TableColumn creator = new TableColumn("Creator");
+        TableColumn runtime = new TableColumn("Runtime");
+        playlists.getColumns().addAll(playlist,creator,runtime);
+
         VBox controlPanel = createControlPanel(gui);
 
         rightVbox.getChildren().add(addPlaylistButton);
@@ -135,9 +168,14 @@ public class GUIBuilder {
     public static VBox createControlPanel(GUI gui)
     {
         VBox controlPanel = new VBox();
+
         HBox playButtons = createPlayButtons();
+
         Button playModeButton = new Button();
+        playModeButton.setText("Shuffle");
+
         ProgressBar songProgress = new ProgressBar();
+
         Slider volumeSlider = new Slider();
 
         controlPanel.getChildren().add(playButtons);
@@ -151,9 +189,15 @@ public class GUIBuilder {
     public static HBox createPlayButtons()
     {
         HBox playButtons = new HBox();
+
         Button rewindButton = new Button();
+        rewindButton.setText("Rewind");
+
         Button pauseButton = new Button();
+        pauseButton.setText("Pause");
+
         Button skipButton = new Button();
+        skipButton.setText("Skip");
 
         playButtons.getChildren().add(rewindButton);
         playButtons.getChildren().add(pauseButton);
@@ -166,10 +210,10 @@ public class GUIBuilder {
     public static HBox createMenuBar(GUI gui)
     {
         HBox menuHbox = new HBox();
+
         MenuBar menuBar = new MenuBar();
         Menu fileMenu = new Menu("File");
         Menu editMenu = new Menu("Edit");
-
         menuBar.getMenus().addAll(fileMenu,editMenu);
 
         menuHbox.getChildren().add(menuBar);
