fa9eb3d20c94e0baea0ca4cc168537d40d923a35
TomDreys <tdreys.61735@farnborough.ac.uk>
2018-01-14 21:54:59 +0000

    added all empty methods to databasefunctionality class

--- src/MainController/DatabaseFunctionality.java
+++ src/MainController/DatabaseFunctionality.java
@@ -1,4 +1,36 @@
 package MainController;
 
 public class DatabaseFunctionality {
+
+    public void addToPlaylist()
+    {
+
+    }
+
+    public void createNewPlaylist()
+    {
+
+    }
+
+    public void loadFromPlaylist()
+    {
+
+    }
+
+    public void loadPlaylists()
+    {
+
+    }
+
+    public void deleteSong()
+    {
+
+    }
+
+    public void deletePlaylist()
+    {
+
+    }
+
+
 }
